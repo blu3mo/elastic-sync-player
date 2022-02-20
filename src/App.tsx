20ba005980d5cc3a1f578e2b9c632abe4e55c0df
Bluemo <shutaro.aoyama@gmail.com>
2022-02-20 17:38:22 +0900

    Implemented elastic playback speed adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
 
+import ReactPlayer from "react-player";
+import {useLocation, useParams} from "react-router-dom";
+import {useInterval} from "usehooks-ts";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const locationParams = useLocation();
+    const videoUrl = locationParams.pathname + locationParams.search;
+
+    const [playing, setPlaying] = React.useState(false)
+    const [playbackRate, setPlaybackRate] = React.useState(1)
+
+    const ref = useRef<ReactPlayer>(null)
+
+    const maxRate = 2
+    const dividingFactor = 250
+    const calculateSpeed = (currentTime: number, maxTime: number) => {
+        const speedOffset = Math.max(0, Math.min(1, (maxTime - currentTime) / dividingFactor))
+        return Math.min(maxRate, 1 + speedOffset);
+    }
+
+    const adjustPlaybackRate = () => {
+        if (!playing) {
+            console.log("skip")
+            return
+        }
+        setPlaying(false)
+        window.setTimeout(function(){
+            setPlaying(playing) //pauseしてplayすると、durationの値が更新される
+            const newRate = calculateSpeed(ref.current?.getCurrentTime()!, ref.current?.getDuration()!)
+            setPlaybackRate(newRate);
+            console.log(`Playback Rate: ${newRate}`);
+        }, 30);
+    }
+
+    const delay = 8000; //in ms
+    useInterval(() => {
+        adjustPlaybackRate()
+    }, delay)
+
+    return (
+        <>
+            <h1>弾性同期Youtubeプレイヤー</h1>
+            <p>配信を遅れて視聴する際に、いい感じに早送りしてくれるプレイヤーです。</p>
+            <p>遅れているほど、再生速度が速くなります。</p>
+            <p>配信視聴中に巻き戻したり停止したりしても、そのうち追いつきます。</p>
+            <p>https://blu3mo.github.io/elastic/{"{配信URL}"} を開くことでその配信が表示されます。</p>
+            <ReactPlayer
+                ref={ref}
+                playing={playing}
+                controls={true}
+                playbackRate={playbackRate}
+                onPlay={() => { setPlaying(true) }}
+                onPause={() => { setPlaying(false) }}
+                url={videoUrl}
+            />
+            <p>今の再生速度: min({maxRate}, 1 + 遅延秒数/{dividingFactor}) = <b>{playbackRate}</b></p>
+        </>
+    )
 }
 
 export default App;
